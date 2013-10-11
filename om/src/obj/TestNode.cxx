039e5ae42d2d0392d7cc28e663d37c4b72750c04
Johannes Lochmann <ubtjolo>
2013-10-11 10:16:18 +0200

    added properties to test node

--- om/src/obj/TestNode.cxx
+++ om/src/obj/TestNode.cxx
@@ -8,6 +8,9 @@ QString TestNode::singular()
 TestNode::TestNode(const QUuid &id)
     : Object(id)
 {
+    (void) createProperty<int>("an integer", 1000);
+    (void) createProperty<double>("a double", 3.14);
+    (void) createProperty<QString>("a string", "hello, world!");
 }
 
 TestNode::~TestNode()
